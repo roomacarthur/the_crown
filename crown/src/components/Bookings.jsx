0e9cc522510bb39ccc562c5f0da788b30b7842b8
roomacarthur <roomacarthur@gmail.com>
2023-07-27 12:50:29 +0100

    Kids Menu updated childrens menu constants

--- crown/src/components/Bookings.jsx
+++ crown/src/components/Bookings.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../styles';
 
 const Bookings = () => {
+  useEffect(() => {
+    const scr=document.createElement("script");
+    scr.src="https://the-crown-1637256869.resos.com/embed/booking/widget.js?ts="+new Date().getTime();
+    document.getElementById("booking-script-2").appendChild(scr);
+  })
   return (
     <>
     <div className={`${styles.flexCenter} bg-background`}>
       <div className={`${styles.boxWidth}`}>
         <section className={`flex flex-col ${styles.paddingY} ${styles.flexCenter} w-full`}>
-          <h1 className={`${styles.heading2} w-full`}>Make A Booking </h1>
+          <h1 className={`${styles.heading2} w-full h-full`}>Make A Booking </h1>
           <a
-            className='border-blue2 font-bold tracking-[1.2px] p-3 my-[60px] rounded-lg bg-blue2 text-white uppercase'
+            className='border-blue2 font-bold tracking-[1.2px] p-3 my-[60px] rounded-lg bg-blue2 text-white hover:text-accent uppercase'
             href="https://the-crown-hawes.resos.com/booking" target="_blank"
           >
             Click Here To Book
           </a>
+          <a id="booking-widget-2" href="https://the-crown-1637256869.resos.com/booking" data-lang="en" data-restaurant-id="fgABrNh5yWMkLxCbN" data-domain="the-crown-1637256869.resos.com">Book a table</a><div id="booking-script-2" className='mt-[10px] text-center text-[70%] opacity-60'>
+              <a target="_blank" href="https://resos.com">Restaurant table management</a></div>
         </section>
       </div>
     </div>
--- crown/src/constants/kids_menu.js
+++ crown/src/constants/kids_menu.js
@@ -2,136 +2,54 @@ import { kidsMeal }from '../assets'
 
 export const Kids_Menu = [
   {
-    name: "Childrens Menu",
+    name: "Children's Menu",
     img: kidsMeal,
-    info: "",
-    footer: "",
+    info: "All children's meals include an ice cream pudding.",
+    footer: "* = Mac & Cheese is available with or without bacon crumb.",
     menu_products: [
-      {
-        title: "Starters",
-        menuItems: [
-          {
-              name: "Chilli & Ginger Goujons",
-              body: "Chicken goujons server with an Asain style slaw and a honey BBQ glaze.",
-              tags: "",
-              price: "8.95",
-          },
-          {
-              name: "Soup Of The Day",
-              body: "Served with warm crusty bread and butter. See our specials board for the days soup.",
-              tags: "Vegan*",
-              price: "6.95",
-          },
-          {
-              name: "Bruschetta",
-              body: "Mixed cherry tomatoes, olives & garlic served on warm crusty bruschetta and topped with a balsamic glaze & basil volute.",
-              tags: "Vegan",
-              price: "7.95 | £14.95 Sharer",
-          },
-          {
-              name: "Stuffed Flat Mushrooms",
-              body: "A flat mushroom stuffed with stilton, onion, spinach and wrapped in bacon. Served on a bed of mixed salad.",
-              tags: "",
-              price: "7.95",
-          },
-        ],
-    },
-    {
-      title: "Burgers",
-      info: "All our burgers come in a brioche bun with skin on chips.",
-      menuItems: [
-        {
-            name: "Crown Burger",
-            body: "Wensleydale beef patty topped with a flat mushroom, fried egg, bacon and grilled cheese. Served with onion rings and homemade slaw.",
-            tags: "",
-            price: "18.95",
-        },
-        {
-            name: "Sticky Korean Burger",
-            body: "",
-            tags: "",
-            price: "18.95",
-        },
-        {
-            name: "Cheese & Bacon Burger",
-            body: "",
-            tags: "",
-            price: "16.50",
-        },
-      ],
-    },
     {
       title: "Mains",
       info: "",
       menuItems: [
         {
-            name: "Fish & Chips",
-            body: "Freshly battered cod served with mushy peas, skin on chips, lemon wedge, mixed salad and homemade tartar sauce.",
+            name: "Chicken Goujons",
+            body: "",
             tags: "",
-            price: "17.00",
+            price: "7.95",
         },
         {
-            name: "Spicy Bean Chilli",
-            body: "Vegan-based tomato bean chilli. Served with rice, tortilla chips and a dollop of sour cream.",
-            tags: "Vegan",
-            price: "16.50",
+            name: "Fish Goujons",
+            body: "",
+            tags: "",
+            price: "7.95",
+        },
+        {
+            name: "Sausage, Chips & Beans",
+            body: "",
+            tags: "",
+            price: "7.95",
         },
         {
             name: "Mac & Cheese",
-            body: "Homemade Bechamel sauce, cheese, bacon crumb and garlic bread.",
+            body: "",
             tags: "Vegetarian*",
-            price: "16.95",
+            price: "7.95",
+        },
+        {
+            name: "Spaghetti Bolognese",
+            body: "",
+            tags: "",
+            price: "7.95",
+        },
+        {
+            name: "Children's Sunday Roast",
+            body: "",
+            tags: "ONLY AVAILABLE ON SUNDAYS",
+            price: "7.95",
         },
       ],
     },
-    {
-      title: "Wraps",
-      info: "All our wraps come with sour cream, fresh guacamole, tortilla chips and mixed salad.",
-      menuItems: [
-        {
-            name: "Chicken Fajita",
-            body: "Chicken, peppers and onions cooked in our special fajita seasoning all wrapped up in a wheat tortilla.",
-            tags: "",
-            price: "10.95",
-          },
-        {
-            name: "Chicken & Cheese Quesadilla",
-            body: "tortilla filled with spiced chicken and cheese grilled till melted.",
-            tags: "",
-            price: "10.95",
-          },
-      ],
-    },
-    {
-      title: "Salads",
-      info: "",
-      menuItems: [
-        {
-            name: "Chicken Caesar",
-            body: "Iceberg lettuce in a caesar dressing. Served with diced bacon, grilled chicken and croutons.",
-            tags: "",
-            price: "14.95",
-          },
-        {
-            name: "Warm Goats Cheese",
-            body: "A Warm goats cheese wheel served on a bed of iceberg and red leaf. With caramelised green apples and red onion with a walnut and orange dressing. Topped with toasted walnuts.",
-            tags: "Vegetarian",
-            price: "15.95",
-          },
-      ],
-    },
-    {
-      title: "Sandwiches",
-      info: "",
-      menuItems: [
-        {
-            name: "Wensleydale & Chutney",
-            body: "Wensleydale cheese and homemade chutney.",
-            tags: "Vegetarian",
-            price: "9.95",
-          },
-      ],
-    },
+    
     ]
   }
 ]
